7ebd5fb1b805415e015423cf0ad3c0763081a5aa
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2020-02-20 16:30:40 -0400

    feat: added config for deployment

--- scripts/server_runner.py
+++ scripts/server_runner.py
@@ -1,5 +1,17 @@
 import uvicorn as asgi
+from os import environ
+
+from src import app
+from .alembic_helper import upgrade_latest
 
 
 def run_app(app, port=8000, host='0.0.0.0'):
-    asgi.run(app, port=port, host=host)
+    return asgi.run(app, port=port, host=host)
+
+
+def run_production():
+    if environ['GENERATE_DATA']:
+        upgrade_latest(generate_data=True)
+    else:
+        upgrade_latest()
+    return run_app(app)
