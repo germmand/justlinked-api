6b492e72bbd4b76272513cfe9ace1ef5388486b0
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2020-02-20 18:14:13 -0400

    fix: error corriendo en producción

--- src/server.py
+++ src/server.py
@@ -5,3 +5,13 @@ from src.schema import schema
 
 app = FastAPI()
 app.add_route("/", GraphQLApp(schema=schema))
+
+
+@app.on_event('startup')
+def upgrade():
+    from os import environ
+    from scripts.alembic_helper import upgrade_latest
+    if environ.get('GENERATE_DATA') and environ.get('MIGRATE'):
+        upgrade_latest(True)
+    elif environ.get('MIGRATE'):
+        upgrade_latest(False)
