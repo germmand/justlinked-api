cc84d1769abcbac7bf075e22a59d5c34210288ea
germmand <german.aguilera17@gmail.com>
2020-02-26 23:44:45 -0400

    fix: Allow CORS all origins.

--- src/server.py
+++ src/server.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from starlette.graphql import GraphQLApp
+from starlette.middleware.cors import CORSMiddleware
 
 from src.schema import schema
 
 app = FastAPI()
 app.add_route("/", GraphQLApp(schema=schema))
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event('startup')
 def upgrade():
