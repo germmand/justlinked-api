13fbf9aee71a62a47d060ffd9cf05ec64d6c968c
germmand <german.aguilera17@gmail.com>
2020-03-09 20:35:53 -0400

    refactor: Create a separate class to handle Google API Requests.

--- src/auth/google/router.py
+++ src/auth/google/router.py
@@ -4,27 +4,20 @@ import requests
 from fastapi import APIRouter
 from starlette.responses import RedirectResponse
 
-from .utils import create_google_auth_server_url
+from .google_oauth import GoogleOAuth2
 
 from src.core.config.config import Config
 
 router = APIRouter()
-
-# Esto se tiene que refactorizar cañon pero ya sirve gg
+redirect_uri = Config.HOST + '/login/google/callback'
+google_auth = GoogleOAuth2(os.environ["GOOGLE_CLIENT_ID"],
+                           os.environ["GOOGLE_CLIENT_SECRET"],
+                           requests,
+                           redirect_uri)
 
 @router.get("/login/google", tags=["external_login"])
 async def get_google_auth_server_url():
-    redirect_uri = Config.HOST + router.url_path_for('handle_google_login_response')
-    client_id = os.environ["GOOGLE_CLIENT_ID"]
-    scopes = ['https://www.googleapis.com/auth/contacts.readonly', 
-              'https://www.googleapis.com/auth/user.addresses.read',
-              'https://www.googleapis.com/auth/user.birthday.read',
-              'https://www.googleapis.com/auth/user.emails.read',
-              'https://www.googleapis.com/auth/user.organization.read',
-              'https://www.googleapis.com/auth/user.phonenumbers.read',
-              'https://www.googleapis.com/auth/userinfo.email',
-              'https://www.googleapis.com/auth/userinfo.profile']
-    google_auth_server_url = create_google_auth_server_url(client_id, redirect_uri, scopes)
+    google_auth_server_url = google_auth.assemble_auth_server_url()
     response = RedirectResponse(url=google_auth_server_url)
     return response
 
@@ -32,23 +25,7 @@ async def get_google_auth_server_url():
 async def handle_google_login_response(code = None, error = None):
     if error is not None:
         return {'error': error}
-    url = 'https://oauth2.googleapis.com/token'
-    redirect_uri = Config.HOST + router.url_path_for('handle_google_login_response')
-    params = {
-        'client_id': os.environ["GOOGLE_CLIENT_ID"],
-        'client_secret': os.environ["GOOGLE_CLIENT_SECRET"],
-        'grant_type': 'authorization_code',
-        'code': code,
-        'redirect_uri': redirect_uri
-    }
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    google_response = requests.post(url=url, data=params, headers=headers)
-    access_token = google_response.json()["access_token"]
-    url = 'https://people.googleapis.com/v1/people/me?personFields=emailAddresses,names,photos,nicknames,birthdays'
-    headers = {
-        'Authorization': 'Bearer {}'.format(access_token)
-    }
-    google_user_data = requests.get(url=url, headers=headers)
+    google_tokens_response = google_auth.get_tokens(code)
+    access_token = google_tokens_response.json()["access_token"]
+    google_user_data = google_auth.get_user_data(access_token, ['emailAddresses', 'names', 'photos', 'nicknames', 'birthdays'])
     return google_user_data.json()
